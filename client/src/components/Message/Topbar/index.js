5d180aa31e727c61a5644e7e50a87696a4e00a36
TasnimJubaier <tasnimjubaier@gmail.com>
2023-05-10 18:16:10 +0600

    Add message topbar call buttons

--- client/src/components/Message/Topbar/index.js
+++ client/src/components/Message/Topbar/index.js
@@ -3,14 +3,33 @@ import React from 'react'
 import styles from './index.module.css'
 
 const Topbar = ({selectedUser}) => {
+
+	const handleCallAudio = (e) => {
+
+	}
+
+	const handleCallVideo = (e) => {
+		
+	}
 	
 	return (
 		<div className={styles['topbar']}>
-			<div className={styles["image-div"]}>
-				
+			<div className={styles['left']}>
+				<div className={styles["image"]}>
+					
+				</div>
+				<div className={styles["title"]}>
+					{selectedUser ? selectedUser.username : "Select a user to start a chat"}
+				</div>
 			</div>
-			<div className={styles['others-div']}>
-				{selectedUser ? selectedUser.username : "Select a user to start a chat"}
+			<div className={styles['right']}>
+				<div className={styles["buttons"]}>
+					<button className={styles['audio-call']} onClick={handleCallAudio}>A Call</button>
+					<button className={styles['video-call']} onClick={handleCallVideo}>V Call</button>
+				</div>
+				<div className={styles["select"]}>
+					S
+				</div>
 			</div>
 		</div>
 	)
