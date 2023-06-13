274372816dbded6f4ef6a800d409387aeef083da
TasnimJubaier <tasnimjubaier@gmail.com>
2023-06-13 23:57:29 +0600

    upload and get image data from imagekit

--- server/src/imagekit/index.ts
+++ server/src/imagekit/index.ts
@@ -1,4 +1,10 @@
 import ImageKit from "imagekit";
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 var publicKey = "public_+rL2s/vADCZ2JzHasIsIzhviuhw="
 var privateKey = "private_bwpoGkut9xMA38xfN1kPU3tPLEU="
@@ -12,60 +18,67 @@ var imagekit = new ImageKit({
 
 
 export const uploadImage = (imagefile, fileName) => {
-    return new Promise((resolve, reject) => {
-        imagekit.upload({
-          file: imagefile,
-          fileName,
-          extensions: [
-            {
-              name: "google-auto-tagging",
-              maxTags: 5,
-              minConfidence: 95,
-            },
-          ],
-        })
-          .then((response) => {
-            console.log(response);
-            resolve(JSON.stringify(response));
-          })
-          .catch((error) => {
-            console.log(error);
-            reject(JSON.stringify(error));
-          });
+  return new Promise((resolve, reject) => {
+    const basePath = path.join(path.resolve(path.resolve(__dirname, '..'), '..'), "src", "imagekit")
+    const inputPath = path.join(basePath, "doll.jpg")
+    const outputPath = path.join(basePath, "dollout.jpg")
+    console.log(inputPath)
+    const buffer = fs.readFileSync(inputPath);
+    fs.writeFileSync(outputPath, buffer)
+
+    imagekit.upload({
+      file: buffer,
+      fileName,
+      extensions: [
+        {
+          name: "google-auto-tagging",
+          maxTags: 5,
+          minConfidence: 95,
+        },
+      ],
+    })
+      .then((response) => {
+        console.log(response);
+        resolve(JSON.stringify(response));
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(JSON.stringify(error));
       });
+  });
 }
 
 
 export const listImages = () => {
-    return new Promise((resolve, reject) => {
-      imagekit.listFiles({
-        skip: 0,
-        limit: 10,
+  return new Promise((resolve, reject) => {
+    imagekit.listFiles({
+      skip: 0,
+      limit: 10,
+    })
+      .then((response) => {
+        console.log(response);
+        resolve(JSON.stringify(response));
       })
-        .then((response) => {
-          console.log(response);
-          resolve(JSON.stringify(response));
-        })
-        .catch((error) => {
-          console.log(error);
-          reject(JSON.stringify(error));
-        });
-    });
-  };
+      .catch((error) => {
+        console.log(error);
+        reject(JSON.stringify(error));
+      });
+  });
+};
   
 
 
 export const getImage = (file) => {
-    return new Promise((resolve, reject) => {
-        imagekit.getFileDetails(file)
-        .then(response => {
-            console.log(response);
-            resolve(JSON.stringify(response))
-        }).catch(error => {
-            console.log(error);
-            reject(JSON.stringify(error))
-        });
-        });
+  return new Promise((resolve, reject) => {
+    imagekit.getFileDetails(file)
+    .then(response => {
+        console.log(response);
+        resolve(JSON.stringify(response))
+    }).catch(error => {
+        console.log(error);
+        reject(JSON.stringify(error))
+    });
+  });
 }
 
 export default imagekit
