ae3438b53a747161d4e91790dff3960708942781
TJubaier <tasnim.jubaier@tigerit.com>
2023-08-20 16:27:25 +0600

    Replace createBrowserRoute with BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import axios from 'axios'
+
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter,
+  Route,
+  Routes,
 } from "react-router-dom";
 
 import Login from "./pages/Login";
@@ -12,83 +11,33 @@ import ChatsSection from "./components/ChatsSection";
 import './App.css'
 import FeedSection from "./components/Feed";
 import Home from './pages/Home'
-import { SelectOptions } from "./utils/constants";
 import EventsSection from "./components/EventsSection";
-import { useLazyQuery } from "@apollo/client";
-import { GET_PLACES_QUERY } from "./utils/queries";
 import PlacesSection from "./components/PlacesSection";
 import UsersSection from "./components/UsersSection";
 import ReelsSection from "./components/ReelsSection";
 
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    children: [
-      {
-        path: SelectOptions.HOME,
-        element: <FeedSection />
-      },
-      {
-        path: SelectOptions.CHATS,
-        element: <ChatsSection />
-      },
-      {
-        path: SelectOptions.REELS,
-        element: <ReelsSection />
-      },
-      {
-        path: SelectOptions.GROUPS,
-        element: <PlacesSection />
-      },
-      {
-        path: SelectOptions.EVENTS,
-        element: <EventsSection />
-      },
-      {
-        path: SelectOptions.USERS,
-        element: <UsersSection />
-      }
-    ]
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  }  
-]);
-
 
 
 function App() {
-  // const [getNearbyPlaces, {data, error}] = useLazyQuery(GET_PLACES_QUERY)
-  // https://developers.google.com/maps/documentation/places/web-service/search-nearby
-  // useEffect(()=> {
-  //   getNearbyPlaces({ variables : {
-  //     location: `23.8670522%2C90.1957362`,
-  //     radius: "1500",
-  //     keyword: "restaurant",
-  //     type: "restaurant"
-  //   }})
-  //   console.log('feching places')
-  // }, [])
-
-  // useEffect(() => {
-  //   if(error) console.log(error)
-  //   if(data) {
-  //     const obj = JSON.parse(data.getNearbyPlaces)
-  //     console.log(obj)
-  //   }
-  // }, [data, error])
 
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="home" element={<FeedSection />} />
+            <Route path="reels" element={<ReelsSection />} />
+            <Route path="users" element={<UsersSection />} />
+            <Route path="chats" element={<ChatsSection />} />
+            <Route path="groups" element={<PlacesSection />} />
+            <Route path="events" element={<EventsSection />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/components/ChatsSection/index.js
+++ client/src/components/ChatsSection/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 
-import NavigationPanel from '../NavigationPanel'
 import Users from './Users'
 import Message from './Message'
 import  styles from './index.module.css'
