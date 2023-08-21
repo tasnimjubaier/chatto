9a91eb0302a7a98f2ebc58dd7e5ab9226b8619db
TJubaier <tasnim.jubaier@tigerit.com>
2023-08-21 16:46:17 +0600

    add more routes

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ChatsSection from "./components/ChatsSection";
 import './App.css'
-import FeedSection from "./components/Feed";
+import FeedSection from "./components/FeedSection";
 import Home from './pages/Home'
 import EventsSection from "./components/EventsSection";
 import PlacesSection from "./components/PlacesSection";
@@ -27,12 +27,19 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}>
+            <Route index element={<FeedSection />} />
             <Route path="home" element={<FeedSection />} />
+            <Route path="feed/:feedId" />
             <Route path="reels" element={<ReelsSection />} />
+            <Route path="reel/:reelId" />
             <Route path="users" element={<UsersSection />} />
+            <Route path="user/:userId" />
             <Route path="chats" element={<ChatsSection />} />
-            <Route path="groups" element={<PlacesSection />} />
+            <Route path="m/:chatId" />
+            <Route path="explore" element={<PlacesSection />} />
+            <Route path="explore/:placeId" />
             <Route path="events" element={<EventsSection />} />
+            <Route path="e/:eventId"  />
           </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
